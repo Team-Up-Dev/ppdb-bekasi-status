9ad09ac1b07e0a3e1a15027416dd0bdc8051fda4
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-01-08 09:49:20 +0700

    Menambahkan fungsi curl data ke bekasi.siap-ppdb.com

--- fetch.py
+++ fetch.py
@@ -1,3 +1,14 @@
-import json
 import os
 import time
+import json
+
+config = open('config.json')
+confData = json.load(config)
+curl = 'curl https://bekasi.siap-ppdb.com/seleksi/reguler/smp/1-22020013-0.json --output json/data.json'
+
+if not os.path.exists(confData['JSON_PATH']):
+    os.makedirs(confData['JSON_PATH'])
+
+while True:
+    time.sleep(confData['REFRESH_DATA'])
+    os.system(curl)
