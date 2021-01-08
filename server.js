a5244cc3438fe32d59a236ff3ae6c7479d38237c
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-01-08 09:11:21 +0700

    Menambahkan beberapa endpoint

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const PUBLIC_PATH = path.resolve("public");
+const NM_PATH = path.resolve("node_modules"); // NM => Node Modules
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(express.static(PUBLIC_PATH));
+app.use("/bs", express.static(path.join(NM_PATH, "bootstrap")));
+app.get("/", (req, res) => res.sendFile(path.join(PUBLIC_PATH, "index.html")));
 
 app.listen(PORT, () =>
   console.log(`Listening on port ${PORT} | http://localhost:${PORT}`)
