844653b3075c1edae1c2ee38318dcfb1c6024a3e
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-01-08 16:06:51 +0700

    Menambahkan socket io dan chokidar

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
+const socketIO = require("socket.io");
+const chokidar = require("chokidar");
 const express = require("express");
 const path = require("path");
+const http = require("http");
+const fs = require("fs");
+
 const app = express();
+
+const config = require(path.resolve("config.json"));
 
 const PORT = process.env.PORT || 3000;
 const PUBLIC_PATH = path.resolve("public");
 const NM_PATH = path.resolve("node_modules"); // NM => Node Modules
+const JSON_PATH = path.resolve(config.JSON_PATH);
+const FULL_PATH = path.join(JSON_PATH, config.OUTPUT_FILE);
+
+if (!fs.existsSync(JSON_PATH)) {
+  fs.mkdirSync(JSON_PATH);
+  const touch = fs.createWriteStream(FULL_PATH);
+  touch.end();
+}
 
 app.use(express.static(PUBLIC_PATH));
 app.use("/bs", express.static(path.join(NM_PATH, "bootstrap")));
 app.get("/", (req, res) => res.sendFile(path.join(PUBLIC_PATH, "index.html")));
 
-app.listen(PORT, () =>
+const server = http.createServer(app);
+server.listen(PORT, () =>
   console.log(`Listening on port ${PORT} | http://localhost:${PORT}`)
 );
+
+const Sock = socketIO(server);
+const watcher = chokidar.watch(FULL_PATH, { persistent: true });
+
+Sock.on("connection", (socc) => {});
